684844af7e823830a28f72fe68966229c3d9d43f
Jinjiang <zhaojinjiang@me.com>
2016-09-20 23:18:30 +0800

    passed timer module test

---
+++ test/unit-module-timer.js
@@ -0,0 +1,60 @@
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+const { expect } = chai
+chai.use(sinonChai)
+
+const timer = require('../lib/modules/timer')
+console.log(timer.setTimeout)
+
+describe('Module: timer', () => {
+  const instance = { id: 'foo', extension: {}}
+  const document = {}
+
+  afterEach(() => {
+    delete instance.$callback
+  })
+
+  it('setTimeout', done => {
+    const begin = Date.now()
+    instance.$callback = (funcId, data, isLast) => {
+      expect(funcId).eql('fooCallback')
+      expect(data).is.null
+      expect(isLast).is.falsy
+      expect(Date.now() - begin).within(400, 600)
+      done()
+    }
+    timer.setTimeout(instance, document, 'fooCallback', 500)
+  })
+
+  it('clearTimeout', done => {
+    const spy = sinon.spy()
+    instance.$callback = spy
+    timer.setTimeout(instance, document, 'fooCallback2', 300)
+    timer.clearTimeout(instance, document, 'fooCallback2')
+    setTimeout(() => {
+      expect(spy.args.length).eql(0)
+      done()
+    }, 600)
+  })
+
+  it('setInterval & clearInterval', done => {
+    const begin = Date.now()
+    let counter = 0
+    instance.$callback = (funcId, data, isLast) => {
+      counter++
+      expect(funcId).eql('fooCallback4')
+      expect(data).is.null
+      expect(isLast).is.truthy
+      expect(Date.now() - begin).within(300 * counter - 100, 300 * counter + 100)
+    }
+    timer.setInterval(instance, document, 'fooCallback4', 300)
+    setTimeout(() => {
+      timer.clearInterval(instance, document, 'fooCallback4')
+    }, 700)
+    setTimeout(() => {
+      expect(counter).eql(2)
+      done()
+    }, 1000)
+  })
+})
