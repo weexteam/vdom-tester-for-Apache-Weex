7480b68706b2e2b2c008473ac213d2dbda4ccff9
Jinjiang <zhaojinjiang@me.com>
2016-09-22 12:13:48 +0800

    use weex-js-framework and passed vanilla framework

--- test/index.js
+++ test/index.js
@@ -6,8 +6,19 @@ const {
   Instance
 } = require('../')
 
+let sendTasksHandler = function () {}
+const { config } = require('weex-js-framework/src/runtime')
+config.sendTasks = config.Document.handler = function () {
+  sendTasksHandler.apply(null, arguments)
+}
+
+
+// init foo framework
 const currentInstanceId = 1
 const fooFramework = {
+  init: function (config) {
+    this.config = config
+  },
   createInstance (id, code) {
     /* eslint-disable no-eval */
     eval('with (this) {' + code + '}')
@@ -20,8 +31,13 @@ const fooFramework = {
   registerMethods () {},
   callJS () {}
 }
+fooFramework.init(config)
 
-describe('Vanilla Test', () => {
+// init vanilla framework
+const vanillaFramework = require('weex-js-framework/src/frameworks/vanilla')
+vanillaFramework.init(config)
+
+describe('Raw Test', () => {
   it('could check real DOM structure in render', () => {
     const runtime = new Runtime(fooFramework)
     const instance = new Instance(runtime)
@@ -39,6 +55,56 @@ describe('Vanilla Test', () => {
       { module: 'dom', method: 'addElement', args: ['_root', { ref: 'first', type: 'text', attr: { value: 'Hello' }}, -1] },
       { module: 'dom', method: 'createFinish', args: [] }
     ])
+    expect(instance.history.refresh.map(task => {
+      return { type: task.type }
+    })).eql([{ type: 'createInstance' }])
+  })
+})
+
+describe('Vanilla Test', () => {
+  it('could check real DOM structure in render', () => {
+    const runtime = new Runtime(vanillaFramework)
+    const instance = new Instance(runtime)
+    sendTasksHandler = function () {
+      runtime.target.callNative.apply(runtime.target, arguments)
+    }
+    const code = [
+      `
+      var body = document.createElement('div', {
+        classStyle: { alignItems: 'center', marginTop: 120 }
+      })
+
+      var image = document.createElement('image', {
+        attr: { src: 'http://alibaba.github.io/weex/img/weex_logo_blue@3x.png' },
+        classStyle: { width: 360, height: 82 }
+      })
+
+      var text = document.createElement('text', {
+        attr: { value: 'Hello World' },
+        classStyle: { fontSize: 48 }
+      })
+
+      body.appendChild(image)
+      body.appendChild(text)
+      document.documentElement.appendChild(body)
+
+      body.addEvent('click', function () {
+        text.setAttr('value', 'Hello Weex')
+      })
+
+      sendTasks(id, [{ module: 'dom', method: 'createFinish', args: []}])
+      `
+    ].join('\n')
+    instance.$create(code)
+    expect(instance.getRealRoot()).eql({
+      type: 'div',
+      style: { alignItems: 'center', marginTop: 120 },
+      event: ['click'],
+      children: [
+        { type: 'image', attr: { src: 'http://alibaba.github.io/weex/img/weex_logo_blue@3x.png' }, style: { width: 360, height: 82 }},
+        { type: 'text', attr: { value: 'Hello World' }, style: { fontSize: 48 }}
+      ]
+    })
     expect(instance.history.refresh.map(task => {
       return { type: task.type }
     })).eql([{ type: 'createInstance' }])
